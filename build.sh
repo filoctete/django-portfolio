863354266970bcd9acde1e33da6946637fd93312
JoaoAmais <joaofreitas@amaiscasa.com>
2026-04-08 23:53:55 +0100

    Fix vercel.json and force migrations in wsgi - V5

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 # build.sh
 pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Criar superuser se não existir
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'freitas.joao2000@email.com', 'senha1') if not User.objects.filter(username='admin').exists() else None"
-python3.12 manage.py collectstatic --noinput
-python3.12 manage.py migrate
