6dfaf98a67cb700d0fee16f8efc9afa602492efe
JoaoAmais <joaofreitas@amaiscasa.com>
2026-04-08 23:23:11 +0100

    added command for build

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 # build.sh
 pip install -r requirements.txt
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'freitas.joao2000@email.com', 'senha1') if not User.objects.filter(username='admin').exists() else None"
 python3.12 manage.py collectstatic --noinput
 python3.12 manage.py migrate
