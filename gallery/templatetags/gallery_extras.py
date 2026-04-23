569084a35b8980ea636a81a6eb23fe396db51043
JoaoAmais <joaofreitas@amaiscasa.com>
2026-04-23 16:35:37 +0100

    last features suggested by client v2

--- gallery/templatetags/gallery_extras.py
+++ gallery/templatetags/gallery_extras.py
@@ -12,6 +12,6 @@ def style_title(value):
         original_bracket = match.group(0) # Ex: (NR 581)
         # Transforma em lowercase e coloca dentro de um span pequeno
         content = original_bracket.lower()
-        styled_bracket = f'<span style="font-size: 0.6em; opacity: 0.8; margin-left: 5px;">{content}</span>'
+        styled_bracket = f'<span style="font-size: 0.6em; opacity: 0.8; margin-left: 5px; text-transform: lowercase;">{content}</span>'
         return mark_safe(value.replace(original_bracket, styled_bracket))
     return value
